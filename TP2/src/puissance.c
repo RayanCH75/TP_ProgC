fefdd833ed14b22eacfbc2b7770c775951e95fbd
RayanCH75 <cherifirayan1109@gmail.com>
2025-04-30 16:27:57 +0200

    Update puissance.c

---
+++ TP2/src/puissance.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+int main() {
+    int a = 2;  // Base
+    int b = 3;  // Exposant
+    int resultat = 1;
+    
+    // Calcul de la puissance
+    for (int i = 0; i < b; i++) {
+        resultat *= a;
+    }
+    
+    printf("%d à la puissance %d = %d\n", a, b, resultat);
+    
+    return 0;
+}
