f5b890cefc76b0a301aef10fd7bef80fba0f5654
RayanCH75 <cherifirayan1109@gmail.com>
2025-04-30 15:10:59 +0200

    Update boucles.c

---
+++ TP1/src/boucles.c
@@ -0,0 +1,40 @@
+
+#include <stdio.h>
+
+int main() {
+    int compteur;
+
+    printf("Entrez une valeur pour compteur (inférieure à 10) : ");
+    fflush(stdout);
+    
+    if (scanf("%d", &compteur) != 1) {
+        printf("Erreur : Vous devez entrer un nombre entier.\n");
+        return 1;
+    }
+
+    if (compteur >= 10 || compteur < 1) {
+        printf("Erreur : compteur doit être un entier strictement compris entre 1 et 9.\n");
+        return 1;
+    }
+
+    printf("\nTriangle avec des boucles for :\n");
+    for (int i = 1; i <= compteur; i++) {
+        for (int j = 1; j <= i; j++) {
+            // Première et dernière ligne : que des étoiles
+            if (i == 1 || i == compteur) {
+                printf("* ");
+            }
+            // Première et dernière position sur la ligne : étoiles
+            else if (j == 1 || j == i) {
+                printf("* ");
+            }
+            // Le reste : des dièses
+            else {
+                printf("# ");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
